e4f77c3c2f8c8660734699d22389086857b9f011
BritneySpears <159101935+centmarde@users.noreply.github.com>
2026-02-06 21:42:03 +0800

    fix: correct script tag syntax in index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<script setup lang="ts">
   import { onMounted, computed } from 'vue'
   import { useLandingController } from '@/controller/landingController'
   import OuterLayoutWrapper from '@/layouts/OuterLayoutWrapper.vue'
